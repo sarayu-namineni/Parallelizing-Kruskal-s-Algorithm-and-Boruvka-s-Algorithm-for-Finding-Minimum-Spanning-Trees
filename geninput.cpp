a7b5592111c04446b48b9e96275c5b275687d77e
ishikasaxena <56605721+ishikasaxena@users.noreply.github.com>
2022-05-02 16:40:06 -0400

    Fixing bugs in geninput file and changing kruskal to C++

--- geninput.cpp
+++ geninput.cpp
@@ -28,7 +28,7 @@ typedef struct edge {
 int main(int argc, char *argv[]){
 	int opt, maxWeight;
    	unsigned int n, m;
-	
+
 	n = 0;
 	m = 0;
 	maxWeight = 1;
@@ -62,6 +62,10 @@ int main(int argc, char *argv[]){
 		fprintf(stderr, "Invalid args: Not possible to place %i edges among %i nodes\n", m, n);
 		exit(EXIT_FAILURE);
 	}
+    if(m < n-1) {
+        fprintf(stderr, "Invalid args: Not possible for graph to be connected with %i edges and %i nodes\n", m, n);
+        exit(EXIT_FAILURE);
+    }
 
 	/*int *adj = (int *)calloc(n*n, sizeof(int));
 	if(adj == NULL){
@@ -72,12 +76,65 @@ int main(int argc, char *argv[]){
 	std::map<unsigned int, std::list<unsigned int>> adjList;
 	std::map<unsigned int, std::list<unsigned int>>::iterator is_u;
 	std::map<unsigned int, std::list<unsigned int>>::iterator is_v;
+
+	std::map<unsigned int, std::list<unsigned int>>::iterator is_i;
+	std::map<unsigned int, std::list<unsigned int>>::iterator is_randEnd;
 	edge_t* edges = (edge_t *)calloc(m, sizeof(edge_t));
 
 	// keep track of number of edges placed
 	unsigned int numEdges = 0;
 	std::default_random_engine generator;
 	std::uniform_int_distribution<unsigned int> distribution(0, UINT_MAX);
+
+    for(unsigned int i = 0; i < n && numEdges < m; i++) {
+        unsigned int randEnd = distribution(generator) % n;
+        while(randEnd == i) {
+            randEnd = distribution(generator) % n;
+        }
+        int w = (distribution(generator) % maxWeight) + 1;
+
+
+		is_i = adjList.find(i);
+		is_randEnd = adjList.find(randEnd);
+
+        if(is_i != adjList.end() && is_randEnd != adjList.end()){
+			auto ie = std::find(std::begin(adjList[i]), std::end(adjList[i]), randEnd);
+			auto randEnde = std::find(std::begin(adjList[randEnd]), std::end(adjList[randEnd]), i);
+			if(ie == adjList[i].end() && randEnde == adjList[randEnd].end()){
+				adjList[i].push_back(randEnd);
+				adjList[randEnd].push_back(i);
+				edges[numEdges].v1 = i;
+				edges[numEdges].v2 = randEnd;
+				edges[numEdges].w = w;
+				numEdges++;
+			} else if (ie == adjList[i].end() || randEnde == adjList[randEnd].end()){
+				printf("Hash map is inconsistent (first one)\n");
+				exit(EXIT_FAILURE);
+			}
+		} else if(is_i != adjList.end()){
+            adjList[i].push_back(randEnd);
+            edges[numEdges].v1 = i;
+            edges[numEdges].v2 = randEnd;
+            edges[numEdges].w = w;
+            numEdges++;
+            adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(randEnd, { i }));
+		} else if(is_randEnd != adjList.end()) {
+            adjList[randEnd].push_back(i);
+            edges[numEdges].v1 = randEnd;
+            edges[numEdges].v2 = i;
+            edges[numEdges].w = w;
+            numEdges++;
+            adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(i, { randEnd }));
+        } else {
+		    adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(i, { randEnd }));
+		    adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(randEnd, { i }));
+		    edges[numEdges].v1 = i;
+		    edges[numEdges].v2 = randEnd;
+		    edges[numEdges].w = w;
+            numEdges++;
+        }
+    }
+
 	while(numEdges < m){
 		unsigned int u = distribution(generator) % n; // chooses node between 0 and n-1
 		unsigned int v = distribution(generator) % n;
@@ -85,7 +142,10 @@ int main(int argc, char *argv[]){
 
 		is_u = adjList.find(u);
 		is_v = adjList.find(v);
-		if(is_u != adjList.end() && is_v != adjList.end()){
+        if (u == v) {
+            continue;
+        }
+        else if(is_u != adjList.end() && is_v != adjList.end()){
 			auto ue = std::find(std::begin(adjList[u]), std::end(adjList[u]), v);
 			auto ve = std::find(std::begin(adjList[v]), std::end(adjList[v]), u);
 			if(ue == adjList[u].end() && ve == adjList[v].end()){
@@ -99,12 +159,22 @@ int main(int argc, char *argv[]){
 				printf("Hash map is inconsistent\n");
 				exit(EXIT_FAILURE);
 			}
-		} else if(is_u != adjList.end() || is_v != adjList.end()){
-			/*
-			printf("Hash map is inconsistent\n");
-			exit(EXIT_FAILURE);
-			*/
-		} else {
+		} else if(is_u != adjList.end()){
+            adjList[u].push_back(v);
+            edges[numEdges].v1 = u;
+            edges[numEdges].v2 = v;
+            edges[numEdges].w = w;
+            numEdges++;
+            adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(v, { u }));
+		} else if(is_v != adjList.end()) {
+            adjList[v].push_back(u);
+            edges[numEdges].v1 = v;
+            edges[numEdges].v2 = u;
+            edges[numEdges].w = w;
+            numEdges++;
+            adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(u, { v }));
+        }
+        else {
 			adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(u, { v }));
 			adjList.insert(std::pair<unsigned int, std::list<unsigned int>>(v, { u }));
 			edges[numEdges].v1 = u;
